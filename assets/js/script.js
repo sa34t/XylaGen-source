0b7856e075b5357401f714044dda7a5b1240aa26
sa34t <LOWSTUDIOSCONTACT@GMAIL.COM>
2024-12-11 16:33:43 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
+document.querySelectorAll('.plan').forEach(plan => {
+    plan.addEventListener('mouseover', () => {
+        plan.style.boxShadow = "0px 0px 15px 5px #f8a51f";
+    });
+    plan.addEventListener('mouseout', () => {
+        plan.style.boxShadow = "none";
+    });
+});
 
